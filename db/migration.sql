c51cb4e8ce7784a0ee9a7153e5ac6eba1ed84037
Zoi <zoilanbond@gmail.com>
2023-12-14 11:30:11 -0800

    Initial backend setup

---
+++ db/migration.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+user_id SERIAL PRIMARY KEY,
+username VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL
+);
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password) VALUES ('zendo', '123');
+INSERT INTO users (username, password) VALUES ('theDude', 'yea');
+INSERT INTO users (username, password) VALUES ('don', 'fam');
+INSERT INTO users (username, password) VALUES ('tony', 'aye');
